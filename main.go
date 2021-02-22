f20d42851ae00f87c9c085d577fb29fdd8514f51
Andrew Nee <ndrewnee@gmail.com>
2021-02-22 14:59:24 +0500

    Get random article from server

--- main.go
+++ main.go
@@ -1,13 +1,34 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
+	"math/rand"
+	"net/http"
 	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
+type Article struct {
+	ID           int    `json:"id"`
+	Title        string `json:"title"`
+	Subtitle     string `json:"subtitle"`
+	Description  string `json:"description"`
+	CanonicalURL string `json:"canonical_url"`
+}
+
 func main() {
+	response, err := http.Get("https://astralcodexten.substack.com/api/v1/archive?sort=top&search=&offset=0&limit=12")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var articles []Article
+	if err := json.NewDecoder(response.Body).Decode(&articles); err != nil {
+		log.Fatal(err)
+	}
+
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("TOKEN"))
 	if err != nil {
 		log.Panic(err)
@@ -21,17 +42,38 @@ func main() {
 	u.Timeout = 60
 
 	updates, err := bot.GetUpdatesChan(u)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	for update := range updates {
-		if update.Message == nil { // ignore any non-Message Updates
+		if update.Message == nil {
 			continue
 		}
 
 		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-		msg.ReplyToMessageID = update.Message.MessageID
+		if update.Message.IsCommand() {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
 
-		bot.Send(msg)
+			switch update.Message.Command() {
+			case "help":
+				msg.Text = "type /random"
+			case "random":
+				text := "Article not found"
+
+				if i := rand.Intn(len(articles)); i < len(articles) {
+					text = articles[i].CanonicalURL
+				}
+
+				msg.Text = text
+			default:
+				msg.Text = "I don't know that command"
+			}
+
+			if _, err := bot.Send(msg); err != nil {
+				log.Printf("[ERROR] Send message failed: %s", err)
+			}
+		}
 	}
 }
