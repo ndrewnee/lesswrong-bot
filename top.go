ea7f176b5d121bbfc5a9321f863add59e718a404
Andrew Nee <ndrewnee@gmail.com>
2021-02-24 03:20:36 +0500

    Hardcode top list from slatestarcodex

--- top.go
+++ top.go
@@ -5,10 +5,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"regexp"
 
 	md "github.com/JohannesKaufmann/html-to-markdown"
-	"github.com/gocolly/colly"
 )
 
 func commandTop() (string, error) {
@@ -40,29 +38,29 @@ func commandTop() (string, error) {
 	return text.String(), nil
 }
 
-var slatePostReg = regexp.MustCompile(`https://slatestarcodex.com/\d{4,}/\d{2,}/\d{2,}`)
+// As https://slatestarcodex.com top posts won't change anymore it's much more effecient to return hardcoded list.
+const TopMessageSlate = `🏆 Top posts
+
+1. [Beware The Man Of One Study](https://slatestarcodex.com/2014/12/12/beware-the-man-of-one-study/)
+
+2. [Meditations on Moloch](https://slatestarcodex.com/2014/07/30/meditations-on-moloch/)
+
+3. [I Can Tolerate Anything Except The Outgroup](https://slatestarcodex.com/2014/09/30/i-can-tolerate-anything-except-the-outgroup/)
+
+4. [Book Review: Albion’s Seed](https://slatestarcodex.com/2016/04/27/book-review-albions-seed/)
+
+5. [Nobody Is Perfect, Everything Is Commensurable](https://slatestarcodex.com/2014/12/19/nobody-is-perfect-everything-is-commensurable/)
+
+6. [The Control Group Is Out Of Control](https://slatestarcodex.com/2014/04/28/the-control-group-is-out-of-control/)
+
+7. [Considerations On Cost Disease](https://slatestarcodex.com/2017/02/09/considerations-on-cost-disease/)
+
+8. [Archipelago And Atomic Communitarianism](https://slatestarcodex.com/2014/06/07/archipelago-and-atomic-communitarianism/)
+
+9. [The Categories Were Made For Man, Not Man For The Categories](https://slatestarcodex.com/2014/11/21/the-categories-were-made-for-man-not-man-for-the-categories/)
+
+10. [Who By Very Slow Decay](https://slatestarcodex.com/2013/07/17/who-by-very-slow-decay/)`
 
 func commandTopSlate(mdConverter *md.Converter) (string, error) {
-	collector := colly.NewCollector()
-	text := bytes.NewBufferString("🏆 Top posts\n\n")
-
-	i := 1
-	collector.OnHTML("div .entry-content a[href]", func(e *colly.HTMLElement) {
-		link := e.Attr("href")
-
-		if link == "https://slatestarcodex.com/2015/04/21/universal-love-said-the-cactus-person/" {
-			return
-		}
-
-		if slatePostReg.MatchString(link) {
-			text.WriteString(fmt.Sprintf("%v. [%s](%s)\n\n", i, e.Text, link))
-			i++
-		}
-	})
-
-	if err := collector.Visit("https://slatestarcodex.com/about/"); err != nil {
-		return "", fmt.Errorf("get slatestarcodex top failed: %w", err)
-	}
-
-	return text.String(), nil
+	return TopMessageSlate, nil
 }
